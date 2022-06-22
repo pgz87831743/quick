8b73e32eddf77fca43ae7e45a20f84e4131e0c8a
pengmaofang <2559288989@qq.com>
2022-06-22 11:40:53 +0800

    添加security

---
+++ security/src/main/java/jx/pgz/security/SecurityStart.java
@@ -0,0 +1,19 @@
+package jx.pgz.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pengmf
+ * @since 2022/6/22
+ */
+@SpringBootApplication
+public class SecurityStart {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SecurityStart.class,args);
+
+    }
+}
