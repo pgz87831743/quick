9bfedb30b3639f2e317768814e42f061817bd729
pengmaofang <2559288989@qq.com>
2022-06-23 10:24:50 +0800

    添加security

---
+++ security/src/test/java/jx/pgz/basic/BasicTest.java
@@ -0,0 +1,40 @@
+package jx.pgz.basic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author pengmf
+ * @since 2022/6/22
+ */
+public class BasicTest {
+
+
+    @Test
+    void passwordEncoderTest() {
+
+
+
+
+
+        Map<String,PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+        encoders.put("sha256", new StandardPasswordEncoder());
+
+        PasswordEncoder passwordEncoder =
+                new DelegatingPasswordEncoder("bcrypt", encoders);
+
+
+        Assertions.assertFalse(passwordEncoder.matches("haha",passwordEncoder.encode("haha")));
+    }
+}
---
+++ security/src/test/java/jx/pgz/security/SecurityStartTest.java
@@ -0,0 +1,11 @@
+package jx.pgz.security;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = {SecurityStart.class})
+public class SecurityStartTest {
+
+}
